e3cb27469e04ddbdde8bb58294fad775f8f5352b
Owen Flood <owenhflood@gmail.com>
2022-09-27 19:35:00 -0400

    add background image and remove stock images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Welcome to Owen Flood</p>
       </header>
+
+      <div className="App-body">{/* <p>MEEEE</p> */}</div>
+
+      <footer className="App-footer">
+        <p>
+          <a href="https://www.freepik.com/free-vector/car-assembly-line-illustration_3296490.htm#query=factory%20line&position=23&from_view=search">
+            Background image
+          </a>{" "}
+          by katemangostar on Freepik <br />
+        </p>
+          Image by{" "}
+          <a href="https://www.freepik.com/free-vector/male-employee-month-concept_7133470.htm#query=employee%20of%20the%20month&position=12&from_view=search">
+            Freepik
+          </a>
+      </footer>
     </div>
   );
 }
