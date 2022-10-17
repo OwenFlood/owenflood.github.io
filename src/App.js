222f5e50f344485ea3738c8b9b43050684f9a9b0
Owen Flood <owenhflood@gmail.com>
2022-10-17 14:35:09 -0400

    move footer to component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import ControlPanel from './ControlPanel';
 import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
@@ -13,18 +14,7 @@ function App() {
 
       <ControlPanel />
 
-      <footer className="App-footer">
-        <p>
-          <a href="https://www.freepik.com/free-vector/car-assembly-line-illustration_3296490.htm#query=factory%20line&position=23&from_view=search">
-            Background image
-          </a>{" "}
-          by katemangostar on Freepik <br />
-        </p>
-        Image by{" "}
-        <a href="https://www.freepik.com/free-vector/male-employee-month-concept_7133470.htm#query=employee%20of%20the%20month&position=12&from_view=search">
-          Freepik
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => (
+  <footer className="App-footer">
+    <p>
+      <a href="https://www.freepik.com/free-vector/car-assembly-line-illustration_3296490.htm#query=factory%20line&position=23&from_view=search">
+        Background image
+      </a>{" "}
+      by katemangostar on Freepik <br />
+    </p>
+    Image by{" "}
+    <a href="https://www.freepik.com/free-vector/male-employee-month-concept_7133470.htm#query=employee%20of%20the%20month&position=12&from_view=search">
+      Freepik
+    </a>
+  </footer>
+)
+
+export default Footer
