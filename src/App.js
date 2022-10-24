f2ddc65b364814d439c322a86eeee25cf0ae200c
Owen Flood <owenhflood@gmail.com>
2022-10-23 21:52:01 -0400

    move old code to deprication and start from scratch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import { useState } from 'react';
 
 import './App.css';
-import ControlPanel from './ControlPanel';
 import Header from './Header';
-import Footer from './Footer';
-import Carousel from './Carousel';
 import { AppProvider } from "./appContext";
 
 const App = () => {
@@ -15,13 +12,6 @@ const App = () => {
       <div className="App">
         <Header />
 
-        <Carousel />
-
-        <img src="headshot.png" alt="frame" className="headshot" />
-
-        <ControlPanel />
-
-        <Footer />
       </div>
     </AppProvider>
   );
