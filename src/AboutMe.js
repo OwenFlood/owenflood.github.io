ee23b93f9181a4d2597c0655015b724123b467d9
Owen Flood <owenhflood@gmail.com>
2022-10-31 18:40:56 -0400

    improve responsiveness of app

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -7,9 +7,10 @@ export default function AboutMe() {
       <div className="mission-statement">
         <div className="mission-intro">
           <div>
-            <h1>Hi, my name is Owen Flood</h1>
+            <h1 className="heading">Hi, my name is Owen Flood</h1>
+            <h3 className="subheading">Mechanical & Robotics Engineer</h3>
             <p>
-              I am an ex-web developer and current fourth-year student at <br />
+              I am a former web developer and current fourth-year student at
               Drexel University studying Mechanical Engineering and Robotics.
             </p>
           </div>
@@ -17,15 +18,16 @@ export default function AboutMe() {
         <img src="headshot.jpg" alt="headshot" className="headshot" />
       </div>
       <p className="blurb">
-        My biggest goal is to use my skills and abilities to contribute towards
-        making the world a more equitable and just place. I love bringing people
-        together from diverse backgrounds to solve issues. Whether it's
-        serving as the president of Drexel Hack4Impact building software for
-        nonprofits and leading boot camps for beginner programmers, or working
-        with the Drexel Urban Growers to build and implement affordable
-        aeroponic systems that can help provide fresh produce to the West
-        Philadelphia year-round, I believe that technology has an undeniable
-        part to play in shaping a safe, equitable, and greener future.
+        My greatest passion is to use my skills and abilities to contribute
+        towards making the world a more equitable and just place. I love
+        bringing people together from diverse backgrounds to solve complex, real
+        world issues. Whether it's serving as the president of Drexel
+        Hack4Impact building software for nonprofits and leading boot camps for
+        beginner programmers, or working with the Drexel Urban Growers to build
+        and implement affordable aeroponic systems that can help provide fresh
+        produce to the West Philadelphia year-round, I believe that technology
+        has a far-reaching, transformative, and important part to play in creating a
+        safe, equitable, and greener future.
       </p>
     </>
   );
